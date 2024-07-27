4ba1683805058bef411d21140711377d4a467d87
kmsu44 <kmsu444@gmail.com>
2024-07-27 19:07:01 +0900

    feat: shadcn/ui 셋팅

--- src/assets/icons/check.tsx
+++ src/assets/icons/check.tsx
@@ -1,6 +1,6 @@
-import { cn } from "@/utils/cn";
+import { cn } from '@/lib/utils';
 
-export default function CheckIcon(props: React.ComponentProps<"svg">) {
+export default function CheckIcon(props: React.ComponentProps<'svg'>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -9,7 +9,7 @@ export default function CheckIcon(props: React.ComponentProps<"svg">) {
       viewBox="0 0 18 16"
       fill="none"
       {...props}
-      className={cn("w-4 h-4 text-status-positive", props.className)}
+      className={cn('h-4 w-4 text-status-positive', props.className)}
     >
       <circle cx="9" cy="8" r="7" fill="currentColor" />
       <path d="M8.3999 11L11.9999 6" stroke="white" strokeLinecap="round" />
--- src/assets/icons/general-search.tsx
+++ src/assets/icons/general-search.tsx
@@ -1,6 +1,6 @@
-import { cn } from "@/utils/cn";
+import { cn } from '@/lib/utils';
 
-export default function GeneralSearchIcon(props: React.ComponentProps<"svg">) {
+export default function GeneralSearchIcon(props: React.ComponentProps<'svg'>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -9,7 +9,7 @@ export default function GeneralSearchIcon(props: React.ComponentProps<"svg">) {
       viewBox="0 0 24 24"
       fill="none"
       {...props}
-      className={cn("w-6 h-6 text-[#292929]", props.className)}
+      className={cn('h-6 w-6 text-[#292929]', props.className)}
     >
       <path
         d="M21 21L16.6569 16.6569M16.6569 16.6569C18.1046 15.2091 19 13.2091 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19C13.2091 19 15.2091 18.1046 16.6569 16.6569Z"

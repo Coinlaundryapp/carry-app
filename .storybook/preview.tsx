50f410cabea8aa1bbf4706ec7e5832ecc0f2a0b8
kmsu44 <kmsu444@gmail.com>
2024-07-29 01:28:47 +0900

    feat: storybook font적용

---
+++ .storybook/preview.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import type { Preview } from '@storybook/react';
+import '../src/app/globals.css';
+import { pretendard } from '../src/app/layout';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div className={pretendard.className}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default preview;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import localFont from 'next/font/local';
 
-const pretendard = localFont({
+export const pretendard = localFont({
   src: '../../public/fonts/PretendardVariable.woff2',
   display: 'swap',
 });

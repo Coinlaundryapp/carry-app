3ea08e4f046b8679c3b3effa6f2bcbcdb11e9ce6
kmsu44 <kmsu444@gmail.com>
2024-07-29 01:29:09 +0900

    feat: alert 컴포넌트

---
+++ src/components/share/Alert/Alert.tsx
@@ -0,0 +1,33 @@
+import { CircleCheckIcon } from '@/components/share/Icons';
+import { cn } from '@/lib/utils';
+import { cva } from 'class-variance-authority';
+import clsx from 'clsx';
+
+const alertVariants = cva('flex px-4 py-3 gap-3 items-center rounded-lg', {
+  variants: {
+    status: {
+      success: 'bg-status-positive',
+      done: 'bg-base-blue-6',
+    },
+  },
+});
+interface AlertProps {
+  status: 'success' | 'done';
+  label: string;
+}
+export default function Alert({ status, label }: AlertProps) {
+  return (
+    <div className="mx-4 mt-4">
+      <div className={cn(alertVariants({ status }))}>
+        <CircleCheckIcon
+          className={clsx('h-[18px] w-[18px] fill-white', {
+            'fill-status-positive': status === 'success',
+            'fill-base-blue-6': status === 'done',
+          })}
+        />
+
+        <p className="font-semibold text-white font-body-2-normal">{label}</p>
+      </div>
+    </div>
+  );
+}

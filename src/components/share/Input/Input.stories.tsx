a4f0403c8f068eb1465de7fc6fc5e48e1b547662
kmsu44 <kmsu444@gmail.com>
2024-07-29 00:14:34 +0900

    fix: Input 대소문자 변경 오류 해결

---
+++ src/components/share/Input/Input.stories.tsx
@@ -0,0 +1,79 @@
+import { useState } from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import { Input, InputProps } from './Input';
+import { CircleCheckIcon, CircleCloseIcon, SearchIcon } from '@/components/share/Icons';
+
+const meta = {
+  title: 'components/Input',
+  component: Input,
+  tags: ['autodocs'],
+  args: {
+    type: 'text',
+    status: 'default',
+    placeholder: 'Placeholder',
+  },
+  decorators: [
+    (Story: React.ComponentType) => (
+      <div className="mx-auto w-96">
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const render = (args: InputProps) => {
+  const [value, setValue] = useState(args.value);
+  args.value = value;
+  args.onClear = fn(() => setValue(''));
+  args.onChange = fn((e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value));
+  return <Input {...args} />;
+};
+
+export const Default: Story = {
+  args: {
+    value: '',
+    status: 'default',
+  },
+  render,
+};
+
+export const Success: Story = {
+  args: {
+    value: 'lorem@ipsum.com',
+    status: 'success',
+    statusMessage: 'Success message',
+    successIcon: <CircleCheckIcon />,
+  },
+  render,
+};
+
+export const Error: Story = {
+  args: {
+    value: 'lorem@ipsum.com',
+    status: 'error',
+    statusMessage: 'Error message',
+    errorIcon: <CircleCloseIcon className="text-status-destructive" />,
+  },
+  render,
+};
+
+export const Done: Story = {
+  args: {
+    value: '유효한 텍스트 입력 완료',
+    status: 'done',
+  },
+  render,
+};
+export const Search: Story = {
+  args: {
+    type: 'text',
+    status: 'default',
+    placeholder: '검색어를 입력하세요',
+    leftIcon: <SearchIcon />,
+  },
+  render,
+};

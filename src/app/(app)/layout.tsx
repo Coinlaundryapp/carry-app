368f8ac7df88da03bb3ad358cd8ec75e2c48129e
kmsu44 <kmsu444@gmail.com>
2024-08-01 00:40:14 +0900

    feat: 스크롤 여부에 따라 스크롤 조작 버튼 렌더 기능

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,24 +1,41 @@
 'use client';
 
+import { useEffect, useRef, useState } from 'react';
+import { debounce } from 'es-toolkit';
 import ScrollUpButton from '@/components/ScrollUpButton';
 import { BottomNavigation } from '@/components/share/BottomNavigation';
-import { useRef } from 'react';
 
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isVisible, setIsVisible] = useState(false);
   const ref = useRef<HTMLDivElement>(null);
   const scrollToTop = () => {
     ref.current?.scrollTo(0, 0);
   };
+  useEffect(() => {
+    const handleScroll = debounce(() => {
+      if (ref.current) {
+        const scrollTop = ref.current.scrollTop;
+        setIsVisible(scrollTop > 0);
+      }
+    }, 100);
+    const divElement = ref.current;
+    if (divElement) {
+      divElement.addEventListener('scroll', handleScroll);
+      return () => {
+        divElement.removeEventListener('scroll', handleScroll);
+      };
+    }
+  }, [isVisible]);
   return (
     <div className="relative mx-auto flex h-dvh max-w-[600px] flex-col justify-between overflow-hidden bg-white">
       <div className="h-full w-full overflow-scroll pb-8" ref={ref}>
         {children}
       </div>
-      <ScrollUpButton onClick={scrollToTop} />
+      {isVisible && <ScrollUpButton onClick={scrollToTop} />}
       <BottomNavigation />
     </div>
   );

ddce94a072a1709214417b49dc360cca73edefbd
sohee-an <soan32@hanmail.net>
2024-08-12 11:11:55 +0900

    style: 공동현관 style수정

--- src/components/address/EntrancePassword.tsx
+++ src/components/address/EntrancePassword.tsx
@@ -63,9 +63,10 @@ const EntrancePassword: React.FC<TProps> = ({ entranceValue, onChange, onExtraIn
                     status="primary"
                     placeholder="예) 뒤쪽 문은 항상 열려있습니다"
                     value={text}
+                    className="mb-2"
                     onChange={(e) => onExtraInfoChange(e.target.value)}
                   />
-                  <p className="mt-4 flex items-center justify-start gap-1 text-xs">
+                  <p className="flex items-center justify-start gap-1 text-xs">
                     <InformationCircleRed />
                     <span>입력한 방법으로 출입이 불가능한 경우, 수거/배송이 어렵습니다.</span>
                   </p>

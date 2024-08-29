306cbffb66cf05eb79f71f5a06e74ed74049e9e2
sohee-an <soan32@hanmail.net>
2024-08-29 18:50:49 +0900

    fix: layout 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import clsx from 'clsx';
 import AuthProvider from '@/providers/AuthProvides';
 import ReactQueryProviders from '@/hooks/useReactQuery';
 import { pretendard } from '@/font/myLocalFont';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Carry',
@@ -19,6 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={clsx(pretendard.className, 'overflow-hidden bg-black')}>
+        <Script
+          type="text/javascript"
+          src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_ID}`}
+          strategy="beforeInteractive"
+        />
         <ReactQueryProviders>
           <AuthProvider>{children}</AuthProvider>
         </ReactQueryProviders>

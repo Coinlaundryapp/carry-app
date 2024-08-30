59dd465ceae9b842b03f3aa00e9e0c0413d49a00
sohee-an <soan32@hanmail.net>
2024-08-30 15:54:51 +0900

    feat:배송지이동하기 버튼  추가

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -51,7 +51,9 @@ import CarIcon from './icon_car.svg';
 import FramIcon from './Frame 26088186.svg';
 import MapMarkerIcon from './map_marker.svg';
 import SelectedMapMarkerIcon from './selected_map_marker.svg';
-import HomeMarkerIcon from "./home_marker.svg"
+import HomeMarkerIcon from './home_marker.svg';
+import UserCurrentMarkerIcon from './userCurrentMarker.svg';
+import MapBackIcon from './map_back.svg';
 
 import HomeMarkerIcon from './home_marker.svg';
 import UserCurrentMarkerIcon from './userCurrentMarker.svg';

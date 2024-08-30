63ef69f7f1242ee7ae01b2a98e766355e513ff04
sohee-an <soan32@hanmail.net>
2024-08-30 15:34:23 +0900

    feat: 배송지 아이콘 추가

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -51,6 +51,7 @@ import CarIcon from './icon_car.svg';
 import FramIcon from './Frame 26088186.svg';
 import MapMarkerIcon from './map_marker.svg';
 import SelectedMapMarkerIcon from './selected_map_marker.svg';
+import HomeMarkerIcon from "./home_marker.svg"
 
 import HomeMarkerIcon from './home_marker.svg';
 import UserCurrentMarkerIcon from './userCurrentMarker.svg';

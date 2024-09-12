c9782112b602f8b13bf5d03cea521c2f000dea2a
kmsu44 <kmsu444@gmail.com>
2024-09-13 00:59:51 +0900

    chore: 배송비 안내 drawer->dialog로 변경

--- src/components/order/DeliveryCostInfoDrawer.tsx
+++ src/components/order/DeliveryCostInfoDrawer.tsx
@@ -2,6 +2,7 @@ import { Drawer, DrawerClose, DrawerContent, DrawerTrigger } from '@/components/
 import Separator from '@/components/share/Separator/Separator';
 import { ExitIcon, QuestionMarkIcon, SelectIcon } from '@assets/icons';
 import { cn } from '@/lib/utils';
+import { Dialog, DialogContent, DialogTrigger } from '@/components/share/ui/dialog';
 
 const DISTANCE_COST_MAP = [
   { label: '100m 이내', cost: '4,000원', maxDistance: 1000 },
@@ -12,11 +13,11 @@ const DISTANCE_COST_MAP = [
 export default function DeliveryCostInfoDrawer({ distance }: Readonly<{ distance: number }>) {
   const cost = DISTANCE_COST_MAP.find((item) => distance <= item.maxDistance)?.cost;
   return (
-    <Drawer scrollLockTimeout={3000}>
-      <DrawerTrigger>
+    <Dialog>
+      <DialogTrigger>
         <QuestionMarkIcon />
-      </DrawerTrigger>
-      <DrawerContent showIndicator={false}>
+      </DialogTrigger>
+      <DialogContent>
         <div className="px-6 py-5">
           <div className="flex items-center justify-between">
             <p className="font-semibold text-label-normal font-headline-1">배송비 안내</p>
@@ -47,12 +48,13 @@ export default function DeliveryCostInfoDrawer({ distance }: Readonly<{ distance
                 <p>{item.cost}</p>
               </div>
             ))}
-            <ul className="list-inside list-disc font-medium text-label-alternative font-caption-1">
+
+            <ul className="space-y-0 font-medium text-label-alternative font-caption-1 [&>li]:relative [&>li]:pl-[8px] [&>li]:before:absolute [&>li]:before:left-0 [&>li]:before:content-['•']">
               <li>1Km 초과할 경우 100m당 200원씩 추가 부가됩니다.</li>
             </ul>
           </div>
         </div>
-      </DrawerContent>
-    </Drawer>
+      </DialogContent>
+    </Dialog>
   );
 }

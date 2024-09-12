b93a716993bda2728d9bcb529df66f82a7c95bbd
kmsu44 <kmsu444@gmail.com>
2024-09-13 02:59:30 +0900

    refactor: dropdown 화면 밖일경우 위로 올라가게 변경

--- src/app/(fullscreen)/payment/[id]/page.tsx
+++ src/app/(fullscreen)/payment/[id]/page.tsx
@@ -170,7 +170,7 @@ export default function PaymentPage({ params }: Readonly<{ params: { id: string 
           )}
         </div>
       </section>
-      <div className="mt-[258px] px-5 pb-[30px]">
+      <div className="mt-[52px] px-5 pb-[30px]">
         <Button state="fillPrimary" size="full">
           결제하기
         </Button>

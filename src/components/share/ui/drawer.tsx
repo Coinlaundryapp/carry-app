f5fbefe1cfc2b2fd7947ad1c2eb2b85ad91f1e1e
MINSU KIM <kmsu444@gmail.com>
2024-09-24 14:56:38 +0900

    QA 요청사항 수정 (#46)
    * fix: toast메세지 하나만 출력되도록 수정
    
    * fix: 주문수거시 가격정보 삭제
    
    * fix: 신발주문수거시 서브타이틀 삭제
    
    * fix: 전체 너비 600px -> 480px로 수정
    
    * refactor: 브라우저에 반응하도록 layout 수정
    
    * remove: 안쓰는 코드 삭제
    
    * Revert "refactor: 브라우저에 반응하도록 layout 수정"
    
    This reverts commit 692d02974c676f6755a7a1ed168c1d451fc2f360.
    
    * chore: build오류로 인한 lockfile 업데이트

--- src/components/share/ui/drawer.tsx
+++ src/components/share/ui/drawer.tsx
@@ -14,21 +14,9 @@ type DrawerProps = React.ComponentProps<typeof DrawerPrimitive.Root> & {
 
 const Drawer = ({
   shouldScaleBackground = true,
-  shouldShowOverlay = true, // 기본값은 오버레이를 표시함
-  closable = true, // 기본값은 닫을 수 있게 함
-  onOpenChange,
   ...props
-}: DrawerProps) => (
-  <DrawerPrimitive.Root
-    shouldScaleBackground={shouldScaleBackground}
-    onOpenChange={(open) => {
-      // closable이 true일 때만 닫기 동작 허용
-      if (closable || open) {
-        onOpenChange?.(open);
-      }
-    }}
-    {...props}
-  />
+}: React.ComponentProps<typeof DrawerPrimitive.Root>) => (
+  <DrawerPrimitive.Root shouldScaleBackground={shouldScaleBackground} {...props} />
 );
 Drawer.displayName = 'Drawer';
 
@@ -68,7 +56,6 @@ const DrawerContent = React.forwardRef<
       ref={ref}
       className={cn(
         'fixed inset-x-0 bottom-0 z-50 mx-auto mt-24 flex h-auto max-w-[480px] flex-col rounded-xl bg-background focus:outline-none',
-
         className,
       )}
       {...props}
